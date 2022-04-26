e50e29014a5868a6638fe57cb11aadffc2b49841
Eemil Aittokoski <86990190+eeait@users.noreply.github.com>
2022-04-26 21:15:19 +0300

    Clean with ESlint and Prettier

--- index.js
+++ index.js
@@ -1,137 +1,103 @@
 const express = require("express")
+
 const app = express()
-const cors = require('cors')
+const cors = require("cors")
 require("dotenv").config()
 const morgan = require("morgan")
 const Person = require("./models/person")
-const { response } = require("express")
 
 app.use(express.static("build"))
 app.use(express.json())
-app.use(morgan(
-    ":method :url :status :res[content-length] - :response-time ms :json"
-))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :json")
+)
+
+// eslint-disable-next-line consistent-return
 morgan.token("json", (req, res) => {
-    if (req.method === "POST") {
-        //console.log(JSON.stringify(res.body))
-        return JSON.stringify(req.body)
-    }
+  if (req.method === "POST") {
+    // console.log(JSON.stringify(res.body))
+    return JSON.stringify(req.body)
+  }
 })
 
-//const { response } = require("express")
 app.use(cors())
 
-let persons = [
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-]
-
 app.get("/api/persons", (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons)
-    })
+  Person.find({}).then((persons) => {
+    res.json(persons)
+  })
 })
 
 app.get("/info", (req, res) => {
-    Person.collection.countDocuments().then((countDocuments) => {
-        res.send(`
-            <p>This phonebook has info for ${countDocuments} people</p>
-            <p>${new Date()}</p>
-        `)
-    })
+  Person.collection.countDocuments().then((countDocuments) => {
+    res.send(`
+      <p>This phonebook has info for ${countDocuments} people</p>
+      <p>${new Date()}</p>
+    `)
+  })
 })
 
-app.get("/api/persons/:id", (req, res) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            res.json(person)
-        })
-        .catch(error => next(error))
-
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      res.json(person)
+    })
+    .catch((error) => next(error))
 })
 
 app.put("/api/persons/:id", (req, res, next) => {
-    const body = req.body
+  const { body } = req
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true})
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.post("/api/persons", (req, res, next) => {
-    const body = req.body
-    //console.log("got POST with body:", body)
+  const { body } = req
 
-    /*//console.log(persons.map(p => p.name))
-    if (persons.map(p => p.name).includes(person.name)) {
-        console.log("NOT UNIQUE")
-        return res.status(400).json({
-            error: "name has to be unique"
-        })
-    }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.id = Math.floor(Math.random() * 1000000)
-    persons = persons.concat(person)*/
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson)
     })
-
-    person.save()
-        .then(savedPerson => {
-            res.json(savedPerson)
-        })
-        .catch(error => next(error))
+    .catch((error) => next(error))
 })
- 
+
 app.delete("/api/persons/:id", (req, res, next) => {
-    /*OLD: const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()*/
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then((result) => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
+// eslint-disable-next-line consistent-return
 app.use((error, req, res, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === "ValidationError") {
-        return res.status(400).json({ error: error.message })
-    }
-    next(error)
+  // console.log(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" })
+  }
+  if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
 })
 
-const PORT = process.env.PORT
+const { PORT } = process.env
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  // console.log(`Server running on port ${PORT}`);
 })
